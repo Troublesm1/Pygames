b4dd70bb746a856ec5c738ca878ee6bc42bad0b9
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-09-04 22:03:15 -0400

    Sprits and  movements

--- PyGame.py
+++ PyGame.py
@@ -1,15 +1,22 @@
 import pygame
 from sys import exit
-
 from pygame import surface
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font\Pixeltype.ttf',50)
 
-sky_surface = pygame.image.load('graphics/sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (70, 300))
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +26,12 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300, 50))
+    screen.blit(player_surf,player_rect)
+    snail_x_pos -= 3
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 267))
+
 
     pygame.display.update()
     clock.tick(60) #Setting the frame rate
