b2b4cdc1baf73f9df30b72ffdf9d999064aa982f
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-09-12 14:40:01 -0400

    Score Rect added

--- PyGame.py
+++ PyGame.py
@@ -10,7 +10,9 @@ test_font = pygame.font.Font('font\Pixeltype.ttf',50)
 
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surf = test_font.render('Score', False, 'Black')
+score_rect = score_surf.get_rect(center =(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect =snail_surf.get_rect(bottomright =(600, 300))
@@ -28,7 +30,7 @@ while True:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface,(300, 50))
+    screen.blit(score_surf,score_rect)
     screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf,player_rect)
     snail_rect.x -=2.5
