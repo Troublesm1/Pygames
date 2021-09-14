42deaa0dbd5d6fc22ca4e8f5551c221e9410cf03
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-09-13 20:30:54 -0400

    Jumping added

--- PyGame.py
+++ PyGame.py
@@ -19,20 +19,21 @@ snail_rect =snail_surf.get_rect(bottomright =(600, 300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (70, 300))
+player_gravity = -20
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        #if event.type == pygame.MOUSEMOTION:
-            #if player_rect.collidepoint(event.pos): print('collison')
+        if event.type == pygame.MOUSEMOTION:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print('jump')
-                
-        if event.type == pygame.KEYUP:
-            print('key up')
+                player_gravity = -20
+        
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
@@ -40,20 +41,13 @@ while True:
     pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
     screen.blit(score_surf,score_rect)
     screen.blit(snail_surf,snail_rect)
-    screen.blit(player_surf,player_rect)
     snail_rect.x -=2.5
     if snail_rect.right <= 0: snail_rect.left = 800
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print('jump')
-
-    #if player_rect.colliderect(snail_rect):
-    #    print('collision')
-    #mouse_pos = pygame.mouse.get_pos()
-    #if player_rect.collidepoint((mouse_pos)):
-    #    print(pygame.mouse.get_pressed())
-
+    #Player
+    player_gravity +=1
+    player_rect.y += player_gravity
+    screen.blit(player_surf,player_rect)
 
     pygame.display.update()
     clock.tick(60) #Setting the frame rate
