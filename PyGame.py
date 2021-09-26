eac4b39e619d9a4da355de811d296e2eb3e92a09
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-09-26 09:52:34 -0400

    Basic Game!

--- PyGame.py
+++ PyGame.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font\Pixeltype.ttf',50)
+game_active = True
 
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
@@ -27,33 +28,39 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEMOTION and player_rect.bottom >= 300 :
-            if player_rect.collidepoint(event.pos):
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -20
+        if game_active:
+            if event.type == pygame.MOUSEMOTION and player_rect.bottom >= 300 :
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    pygame.draw.rect(screen,'#c0e8ec',score_rect,)
-    pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-    screen.blit(score_surf,score_rect)
-    screen.blit(snail_surf,snail_rect)
-    snail_rect.x -=2.5
-    if snail_rect.right <= 0: snail_rect.left = 800
+    if game_active:
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        pygame.draw.rect(screen,'#c0e8ec',score_rect,)
+        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        screen.blit(score_surf,score_rect)
+        screen.blit(snail_surf,snail_rect)
+        snail_rect.x -=4.5
+        if snail_rect.right <= 0: snail_rect.left = 800
 
-    # PLAYER
-    player_gravity +=1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300: player_rect.bottom = 300
-    screen.blit(player_surf,player_rect)
+        # PLAYER
+        player_gravity +=1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf,player_rect)
 
-    # COLLISION
-
-    if snail_rect.colliderect(player_rect):
-        pygame.quit()
-        exit()
+        # COLLISION
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+            screen.fill('Red')
 
     pygame.display.update()
     clock.tick(60) #Setting the frame rate
