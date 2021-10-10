87408b4ba8524c7345d99666701a587f9f23b8a8
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-10-10 10:20:02 -0400

    enemy spawn logic started

--- PyGame.py
+++ PyGame.py
@@ -1,6 +1,6 @@
 import pygame
 from sys import exit
-from pygame import surface
+from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
@@ -8,6 +8,16 @@ def display_score():
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf,score_rect)
     return current_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            screen.blit(snail_surf,obstacle_rect)
+
+        return obstacle_list
+    else: return []
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -25,8 +35,11 @@ ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 # score_surf = test_font.render('Score', False, (64,64,64))
 # score_rect = score_surf.get_rect(center =(400, 50))
 
+#OBSTACLES
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright =(600,300))
+
+obstacle_rect_list = []
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (70,300))
@@ -40,7 +53,7 @@ player_stand_rect = player_stand.get_rect(center = (400,200))
 game_name = test_font.render('Pixel Man',False,(111,196,169))
 game_name_rect = game_name.get_rect(center = (400,80))
 
-#Timer
+#TIMER
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 900)
 
@@ -66,7 +79,7 @@ while True:
                 start_time = int(pygame.time.get_ticks() / 1000)
 
         if event.type == obstacle_timer and game_active:
-            print('test')
+            obstacle_rect_list.append(snail_surf.get_rect(bottomright =(randint(900,1100),300)))
 
     if game_active:
         screen.blit(sky_surface, (0,0))
@@ -76,9 +89,12 @@ while True:
         # screen.blit(score_surf,score_rect)
         score = display_score()
 
-        snail_rect.x -=3.8
-        if snail_rect.right <= 0: snail_rect.left = 800
-        screen.blit(snail_surf,snail_rect)
+        # snail_rect.x -=3.8
+        # if snail_rect.right <= 0: snail_rect.left = 800
+        # screen.blit(snail_surf,snail_rect)
+
+        #OBSTACLE MOVEMENT
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
         # PLAYER
         player_gravity +=1
