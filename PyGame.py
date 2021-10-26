2e0cd7e99808c0b595b65f0873c6a9559d4dab6f
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-10-25 22:26:35 -0400

    Final

--- PyGame.py
+++ PyGame.py
@@ -19,10 +19,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom = (80, 300))
         self.gravity = 0
 
+        self.jump_sound = pygame.mixer.Sound('audio/audio_jump.mp3')
+        self.jump_sound.set_volume(0.3)
+
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys [pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
@@ -108,7 +112,6 @@ def collision_sprite():
         return False
     else: return True
 
-
 def player_animation():
     global player_surf, player_index
 
@@ -128,6 +131,8 @@ test_font = pygame.font.Font('font/Pixeltype.ttf',50)
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.play(loops = -1)
 
 #GROUPS
 player = pygame.sprite.GroupSingle()
